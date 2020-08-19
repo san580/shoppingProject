a52b8cee922e771136e80e56c5ee053e5752c9ec
san580 <sharmasandhya5159@gmail.com>
2020-08-19 17:12:03 -0500

    Changes made while creating java entity

---
+++ src/main/java/com/shopping/model/Employee2.java
@@ -0,0 +1,29 @@
+package com.shopping.model;
+
+public class Employee2 {
+   
+	private String name;
+	private int number;
+	public Employee2(String name, int number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+	
+	
+	
+}
